5e153718e233af5aa460f0e366bb297b8fac11f4
viet <chaunguyenit999@gmail.com>
2023-04-06 15:28:05 +0700

    merge Long and Viet

--- api/controller/dataController.js
+++ api/controller/dataController.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const mongoose = require('mongoose');
 
 const dataAirQuality = require("../model/models.js");
@@ -98,6 +99,79 @@ const dataController = {
                 res.status(500).json({message: error});
             }
         },    
+=======
+const dataAirQuality = require("../models/airQuality-schema");
+const excelToJson = require('convert-excel-to-json');
+const fs = require('fs');
+const dataController = {
+    getData : async(req, res) =>{
+    try{
+        const data = await dataAirQuality.findOne({_id:"642a5a84b77cb5936c77737e"})
+        res.status(200).json(data.humidity)
+    }catch(err){
+        res.status(500).json({message: err});
+    }
+    },
+    insertData: async(req, res) =>{
+        try {
+            const result = excelToJson({
+                source: fs.readFileSync('Không_Khí_2018.xlsx'),
+                  columnToKey: {
+                    A:'location.address',
+                    B:'location.latitude',
+                    C:"location.longitude",
+                    D:"date",
+                    E:"wind_degree",
+                    F:"humidity",
+                    G:"wind_speed",
+                    H:"wind_direction",
+                    I:"pressure",
+                    J:"wind_dust",
+                    K:"sulfur_dioxide",
+                    L:"carbon_monoxide",
+                    M:"nito_dioxit",
+                    N:"equivalent_noise",
+                    O:"extreme_noise",
+                  }
+              });
+        const data = await dataAirQuality.insertMany(result.data1)
+        res.status(200).json(data)
+        }catch (error) {
+            res.status(500).json({message:error.message});
+        }
+    },
+    updateData: async(req, res) => {
+        try{
+            updateData = await dataAirQuality.updateOne(
+                {
+                    location:{
+                        address:"Ha Tay",
+                        latitude:324,
+                        longitude:342
+                }
+                },
+                {
+                    location:{
+                        address:"Ha NAm",
+                        latitude:324,
+                        longitude:342
+                }
+                }
+            );
+            res.status(200).json(updateData);    
+        }catch (error) {
+            res.status(500).json({message: error});
+        }
+    },data_partition: async(req, res)=>{
+        try {
+            const data = await dataAirQuality.find({})
+            for(let i=0; i<data.length; i++) {
+                console.log(data[i])
+            }
+            res.send('Ok')
+        }catch(e){}
+    }
+>>>>>>> long
 }
 
-module.exports = dataController;
+module.exports = dataController
--- api/routes/dataRoutes.js
+++ api/routes/dataRoutes.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const router = require('express').Router();
 const { getAll, getOne, deleteOne, update, update2, insert, insert2} = require('../controller/dataController.js');
 
@@ -14,6 +15,30 @@ function route(app) {
 
     app.get('/insert', insert);
     app.post('/insert2', insert2);  
+=======
+// const route = require('express').Router();
+const { getData, updateData, insertData, data_partition} = require('../controller/dataController.js');
+const axios = require('axios');
+
+
+function route(app) {
+    app.get('/data', getData);
+    app.get('/update', updateData)
+    app.get('/insert', insertData);
+    app.get('/apiweather', async (req, res) => {
+        const { city } = req.query;
+      
+        const url = `http://api.weatherapi.com/v1/current.json?key=517d7a89bee7468699261933230304&q=${city}`;
+      
+        try {
+          const response = await axios.get(url);
+          res.send(response.data);
+        } catch (error) {
+          res.status(404).send(`${city} is not found`);
+        }
+      });
+    app.get('/datapartition', data_partition)
+>>>>>>> long
 }
 
 module.exports = route
