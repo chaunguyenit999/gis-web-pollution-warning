2fdf394a4389451e34aeb8e37ac2f5e9a676da98
Shinras <longcanh2k3@gmail.com>
2023-04-08 19:12:56 +0700

    new Schema

--- api/controller/dataController.js
+++ api/controller/dataController.js
@@ -1,16 +1,15 @@
 const dataAirQuality = require("../models/airQuality-schema");
 const excelToJson = require('convert-excel-to-json');
 const fs = require('fs');
-const dataController = {
-    getData : async(req, res) =>{
+    const getData = async(req, res) =>{
     try{
-        const data = await dataAirQuality.findOne({_id:"642a5a84b77cb5936c77737e"})
-        res.status(200).json(data.humidity)
+        const data = await dataAirQuality.find({})
+        res.status(200).json(data)
     }catch(err){
         res.status(500).json({message: err});
     }
-    },
-    insertData: async(req, res) =>{
+    }
+    const insertData = async(req, res) =>{
         try {
             const result = excelToJson({
                 source: fs.readFileSync('Không_Khí_2018.xlsx'),
@@ -37,8 +36,8 @@ const dataController = {
         }catch (error) {
             res.status(500).json({message:error.message});
         }
-    },
-    updateData: async(req, res) => {
+    }
+    const updateData = async(req, res) => {
         try{
             updateData = await dataAirQuality.updateOne(
                 {
@@ -60,7 +59,8 @@ const dataController = {
         }catch (error) {
             res.status(500).json({message: error});
         }
-    },data_partition: async(req, res)=>{
+    }
+    const data_partition = async(req, res)=>{
         try {
             const data = await dataAirQuality.find({})
             for(let i=0; i<data.length; i++) {
@@ -69,6 +69,6 @@ const dataController = {
             res.send('Ok')
         }catch(e){}
     }
-}
 
-module.exports = dataController
+
+module.exports = {insertData, updateData, data_partition, getData}
