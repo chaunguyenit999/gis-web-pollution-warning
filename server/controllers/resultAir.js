828116e0d90e3008003001d1d2abab450f00717e
Different27 <nguyenviet2k72k3@gmail.com>
2023-04-14 22:14:03 +0700

    new commit

--- server/controllers/resultAir.js
+++ server/controllers/resultAir.js
@@ -4,66 +4,58 @@ const resultAir = {
     result: async (id) => {
         try {
           const res = [];
-          const air= await Air.findById(id);
-          let res1, res2, res3, res4
-          //Bụi mịn
+          const air = await Air.findById(id);
+          let res1, res2, res3
+          //Bụi tổng
           /*
-            1.Tốt nhất: nồng độ PM2.5 dưới 10 µg/m³
-            2.Tốt: nồng độ PM2.5 từ 10 đến 25 µg/m³
-            3.Trung bình: nồng độ PM2.5 từ 25 đến 50 µg/m³
-            4.Kém: nồng độ PM2.5 từ 50 đến 75 µg/m³
-            5.Xấu: nồng độ PM2.5 trên 75 µg/m³
+            1.Tốt: dưới 200 µg/m³
+            2.Hơi tốt: từ 200 đến 300 µg/m³
+            3.Trung bình: từ 300 đến 400 µg/m³
+            4.Kém: từ 400 µg/m³
 */
-          if (air.wind_dust < 10) {
+          if (air.wind_dust < 200) {
             res1 = 1
-          }else if(10<= air.wind_dust && air.wind_dust < 25) {
+          }else if( 200<= air.wind_dust && air.wind_dust < 300) {
             res1 = 2
-          }else if(25<= air.wind_dust && air.wind_dust < 50) {
+          }else if(300 <= air.wind_dust && air.wind_dust < 400) {
             res1 = 3
-          }else if(50<= air.wind_dust && air.wind_dust < 75) {
-            res1 = 4
-          }else if(air.wind_dust >= 75) {
-            res1 = 5 }
+          }else if( air.wind_dust >= 400) {
+            res1 = 4 }
      
           //Sulfur_dioxide
           /*
-            1.Cấp độ tốt: Lượng SO2 trong không khí là từ 0 đến 35 microgram/m3.
-            2.Cấp độ trung bình: Lượng SO2 trong không khí là từ 35 đến 75 microgram/m3.
-            3.Cấp độ không tốt cho sức khỏe: Lượng SO2 trong không khí là từ 75 đến 185 microgram/m3.
-            4.Cấp độ nguy hại cho sức khỏe: Lượng SO2 trong không khí là từ 185 đến 304 microgram/m3.
-            5.Cấp độ rất nguy hại cho sức khỏe: Lượng SO2 trong không khí vượt quá 304 microgram/m3.
+            1.Cấp độ tốt: Lượng SO2 trong không khí là từ 0 đến 30 microgram/m3.
+            2.Cấp độ trung bình: Lượng SO2 trong không khí là từ 30 đến 70 microgram/m3.
+            3.Cấp độ không tốt cho sức khỏe: Lượng SO2 trong không khí là từ 70 đến 180 microgram/m3.
+            4.Cấp độ nguy hại cho sức khỏe: Lượng SO2 trong không khí là từ 180 đến 300 microgram/m3.
 */
-          if (air.sulfur_dioxide < 35) {
+          if (air.sulfur_dioxide < 50) {
             res2 = 1
-          }else if(35<= air.sulfur_dioxide && air.sulfur_dioxide < 75) {
+          }else if( 50 <= air.sulfur_dioxide && air.sulfur_dioxide < 70) {
             res2 = 2
-          }else if(75<= air.sulfur_dioxide && air.sulfur_dioxide < 185) {
+          }else if(70 <= air.sulfur_dioxide && air.sulfur_dioxide < 100) {
             res2 = 3
-          }else if(185<= air.sulfur_dioxide && air.sulfur_dioxide < 304) {
-            res2 = 4
-          }else if(air.sulfur_dioxide >= 304) {
-            res2 = 5 }
+          }else if( air.sulfur_dioxide >= 100) {
+            res2 = 4 }
 
           //nito_dioxit
           /*
-            1.Tốt: nồng độ NO2 dưới 40 µg/m³.
-            2.Trung bình: nồng độ NO2 từ 40 đến 100 µg/m³.
+            1.Tốt: nồng độ NO2 dưới 35 µg/m³.
+            2.Trung bình: nồng độ NO2 từ 35 đến 100 µg/m³.
             3.Kém: nồng độ NO2 từ 100 đến 200 µg/m³.
             4.Nguy hại cho sức khỏe: nồng độ NO2 từ 200 đến 400 µg/m³.
             5.Rất nguy hại cho sức khỏe: nồng độ NO2 trên 400 µg/m³.
 */
-          if (air.nito_dioxit < 40) {
-            res4 = 1
-          }else if(40<= air.nito_dioxit && air.nito_dioxit < 100) {
-            res4 = 2
-          }else if(100<= air.nito_dioxit && air.nito_dioxit < 200) {
-            res4 = 3
-          }else if(200<= air.nito_dioxit && air.nito_dioxit < 400) {
-            res4 = 4
-          }else if(air.nito_dioxit >= 400) {
-            res4 = 5 }
+          if (air.nito_dioxit < 35) {
+            res3 = 1
+          }else if(35 <= air.nito_dioxit && air.nito_dioxit < 50) {
+            res3 = 2
+          }else if(50 <= air.nito_dioxit && air.nito_dioxit < 80) {
+            res3 = 3
+          }else if( air.nito_dioxit >= 80) {
+            res3 = 4 }
   
-        res.push(res1, res2, res4)
+        res.push(res1, res2, res3)
 
         const maxNum = Math.max(...res);
         
